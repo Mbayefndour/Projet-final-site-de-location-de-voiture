e12890a9929f51e5aee20d8f0690ec92602edc95
Mbaye Faye ndour <mbfndour@gmail.com>
2024-03-08 11:14:54 +0000

    first commit

---
+++ Pages/script.js
@@ -0,0 +1,22 @@
+
+//icone de recherche
+let search = document.querySelector('.box-search')
+ 
+document.querySelector('#search').onclick = () => {
+    search.classList.toggle('active');
+}
+ 
+
+//bouton decouvrir
+ document.getElementsByClassName('btn').onclick = ()=>{
+        btn.addEventListener();
+ };
+
+
+//header
+let header  =  document.querySelector('header');
+window.addEventListener('scroll',  () => {
+    header.classList.toggle('shadow' , window.scrollY > 0); 
+
+}) 
+
